3a9df8c46e02ae21048cb99ffc643122b86575d4
L'etranger <92956390+Letranger18@users.noreply.github.com>
2024-10-05 00:45:17 +0800

    Update playground.js

--- Javascript/playground.js
+++ Javascript/playground.js
@@ -1,51 +1,39 @@
-/*
-Assessment Requirements
-1. Create a variable that can hold a number of NFT's. What type of variable might this be?
-2. Create an object inside your mintNFT function that will hold the metadata for your NFTs. 
-   The metadata values will be passed to the function as parameters. When the NFT is ready, 
-   you will store it in the variable you created in step 1
-3. Your listNFTs() function will print all of your NFTs metadata to the console (i.e. console.log("Name: " + someNFT.name))
-4. For good measure, getTotalSupply() should return the number of NFT's you have created
-*/
-
-// create a variable to hold your NFT's
+// Array to store the NFTs
 const NFTs = [];
 
-// this function will take in some values as parameters, create an
-// NFT object using the parameters passed to it for its metadata, 
-// and store it in the variable above.
-function mintNFT (_name, _eyeColor, _shirtType, _bling) {
+// NFT objects with its metadata
+function mintNFT (_name, _type, _symbol, _collection) {
     const NFT = {
         "name": _name,
-        "eyeColor": _eyeColor,
-        "shirtType": _shirtType,
-        "bling": _bling
+        "type": _type,
+        "symbol": _symbol,
+        "collection": _collection
     }
     NFTs.push(NFT);
-    console.log("Minted: " + _name);
 }
 
-// create a "loop" that will go through an "array" of NFT's
-// and print their metadata with console.log()
+// loop that reads and displays the NFTs information from the array
 function listNFTs () {
     for(let i = 0; i < NFTs.length; i++){
-        console.log("\nID: \t\t" + (i + 1));
-        console.log("Name: \t\t" + NFTs[i].name);
-        console.log("Eye Color: \t" + NFTs[i].eyeColor);
-        console.log("Shirt Type: " + NFTs[i].shirtType);
-        console.log("Bling: \t\t" + NFTs[i].bling);
+        console.log("\nNFT: \t\t\t" + NFTs[i].name);
+        console.log("ID: \t\t\t" + "24JB" + (i + 1));
+        console.log("TYPE: \t\t\t" + NFTs[i].type);
+        console.log("SYMBOL: \t\t" + NFTs[i].symbol);
+        console.log("COLLECTION: \t" + NFTs[i].collection);
     }
 }
 
-// print the total number of NFTs we have minted to the console
+// Display the total amount of minted NFTs
 function getTotalSupply() {
-    console.log("\n"+ NFTs.length);
+    console.log("\nYOU HAVE MINTED "+ NFTs.length + " COINS");
 }
 
-// call your functions below this line
-mintNFT("Bob", "Blue", "Hoodie", "Gold Chain");
-mintNFT("Sue", "Blue", "Hoodie", "Gold Chain");
-mintNFT("Tim", "Blue", "Hoodie", "Gold Chain");
-mintNFT("Jogn", "Blue", "Hoodie", "Gold Chain");
+// Function call
+console.log("====================== NFT COLLECTIONS ======================");
+mintNFT("damascus-sword-20", "Diamond", "Long Blade", "2020 Manila JB-NFT Convention");
+mintNFT("spartan-shield-01a", "Silver", "Circle Shield", "Glorious Infantry");
+mintNFT("demolisher-iv", "Silver", "Catapult", "MechaMachines - Spring 2021");
+mintNFT("scottish-offense", "Bronze", "Long Blade", "2020 Manila JB-NFT Convention");
 listNFTs();
+console.log("=============================================================");
 getTotalSupply();
